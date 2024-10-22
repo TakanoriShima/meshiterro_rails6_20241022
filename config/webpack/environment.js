a6e3321c9c41858c8ef147156c17cefad08948d9
EC2 Default User <ec2-user@ip-172-31-33-120.ap-northeast-1.compute.internal>
2024-10-22 06:37:21 +0000

    fin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// 以下を追加
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
