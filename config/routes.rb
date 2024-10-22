afdc20d5fb09498ee92d89b4b737b23b37099ab7
EC2 Default User <ec2-user@ip-172-31-33-120.ap-northeast-1.compute.internal>
2024-10-22 03:25:23 +0000

    Chapter 6 ユーザ詳細ページ・プロフィール編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-    # ---- 以下を追加してください ---- #
-    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-      resources :post_comments, only: [:create, :destroy]
-      resource :favorites, only: [:create, :destroy]
-    end
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
